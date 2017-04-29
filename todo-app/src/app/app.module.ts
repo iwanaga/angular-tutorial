1c4180581d5ab183a64efa2792ed65807327c48d
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-04-29 22:29:41 +0900

    generated via angular-cli

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormComponentComponent } from './form-component/form-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ todo-app/src/app/form-component/form-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-component',
+  templateUrl: './form-component.component.html',
+  styleUrls: ['./form-component.component.css']
+})
+export class FormComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
