7022cbbd6abe10f75a656166b653bf8f312141ca
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-04-30 01:09:25 +0900

    双方向バインディングの例

--- todo-app/src/app/form/form.component.ts
+++ todo-app/src/app/form/form.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  task: Task = {
+    id: 1,
+    name: '掃除',
+    updatedAt: new Date()
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
+  setUpdatedAt() {
+    this.task.updatedAt = new Date();
+  }
+
+}
+
+export class Task {
+  id: number;
+  name: string;
+  updatedAt: Date;
 }
